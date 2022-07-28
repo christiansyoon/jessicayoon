cf87b0ffc06855403cd73db037c9adb3e3e12c6b
Christian Yoon <christianyoon@gmail.com>
2022-07-27 20:43:23 -0400

    Some work on the contacts page.

--- src/App.tsx
+++ src/App.tsx
@@ -5,17 +5,19 @@ import { StyledEngineProvider } from '@mui/material/styles';
 
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
+import ContactPage from './pages/Contact';
+
 const App = () => {
 	return (
 		<StyledEngineProvider injectFirst>
 			<BrowserRouter>
 				<MainMenu/>
 				<Routes>
-					<Route path="/" element={<div style={{ margin: "5px" }}>HOME</div>}/>
-					<Route path="/about" element={<div style={{ margin: "5px" }}>ABOUT</div>}/>
-					<Route path="/blog" element={<div style={{ margin: "5px" }}>BLOG</div>}/>
-					<Route path="/contact" element={<div style={{ margin: "5px" }}>CONTACT</div>}/>
-					<Route path="*" element={<div/>} />
+					<Route index element={<div style={{ margin: "5px" }}>HOME</div>}/>
+					<Route path="about" element={<div style={{ margin: "5px" }}>ABOUT</div>}/>
+					<Route path="blog" element={<div style={{ margin: "5px" }}>BLOG</div>}/>
+					<Route path="contact" element={<ContactPage/>}/>
+					<Route path="*" element={<div/>}/>
 				</Routes>
 			</BrowserRouter>
 		</StyledEngineProvider>
--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,11 +1,61 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const AboutPage = (props) => {
+import { TextField, TextareaAutosize } from '@mui/material';
+
+const ContactPage = (props) => {
+	const [firstName, setFirstName] = useState("");
+	const [lastName, setLastName] = useState("");
+	const [email, setEmail] = useState("");
+	const [message, setMessage] = useState("");
+
+	const handleChange = (newValue, type) => {
+		let newVal = newValue;
+		if (newValue === undefined || newValue === null) {
+			newVal = "";
+		}
+
+		switch(type) {
+			case "firstName":
+				setFirstName(newVal);
+				break;
+			case "lastName":
+				setLastName(newVal);
+				break;
+			case "email":
+				setEmail(newVal);
+				break;
+			case "message":
+				setMessage(newVal);
+				break;
+		}
+	}
 
 	return (
-		<div>
-		</div>
+		<form
+			style={{
+				display: "flex",
+				flexDirection: "column",
+				alignItems: "flex-start",
+				marginTop: "20px",
+				rowGap: "10px"
+			}}
+		>
+			<TextField id="outlined-basic" label="First Name" variant="outlined" value={firstName} onChange={(event) => {
+				handleChange(event.target.value, "firstName");
+			}}/>
+			<TextField id="outlined-basic" label="Last Name" variant="outlined" value={lastName} onChange={(event) => {
+				handleChange(event.target.value, "lastName");
+			}}/>
+			<TextField id="outlined-basic" label="Email" variant="outlined" value={email} onChange={(event) => {
+				handleChange(event.target.value, "email");
+			}}/>
+			<TextareaAutosize id="outlined-basic" minRows={10} style={{ width: 400 }} placeholder="Enter Message" value={message}
+				onChange={(event) => {
+					handleChange(event.target.value, "message");
+				}}
+			/>
+		</form>
 	);
 }
 
-export default AboutPage;
+export default ContactPage;
